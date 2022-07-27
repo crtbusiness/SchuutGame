1e8bb123c0ba69a3ffe030dd18c33a5c71c13c54
Kelvak <31416544+crtbusiness@users.noreply.github.com>
2022-07-27 16:48:21 -0400

    Made more progress, added keypresses, bullet logic

--- main.py
+++ main.py
@@ -28,13 +28,19 @@ def getHeight():
     return int(toInt) #convert toInt string to an int
 
 
-WIDTH = getWidth()/2
-HEIGHT = getHeight()/2
+WINDOW_WIDTH = getWidth()/2
+WINDOW_HEIGHT = getHeight()/2
 #print(WIDTH, HEIGHT)
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+BORDER = pygame.Rect(WINDOW_WIDTH/2 - 5, 0, 10, WINDOW_HEIGHT)
 
 FPS = 60
+VEL = 5 #Velocity for spaceship to move
+BULLET_VEL = 7
+MAX_BULLETS = 4
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
@@ -44,17 +50,44 @@ YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESH
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("First Game!")        #Sets the window name
 
-def draw_window():
+def draw_window(red, yellow):
     WIN.fill(WHITE)     #Sets the background color of the window, uses RGB
-    WIN.blit(YELLOW_SPACESHIP, (WIDTH*1/4, HEIGHT/2))       #Places image above the background layer at (x,y) coordinates
-    WIN.blit(RED_SPACESHIP, (WIDTH*3/4, HEIGHT/2))
+    pygame.draw.rect(WIN, BLACK, BORDER)
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))       #Places image above the background layer at (x,y) coordinates
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()     #Updates display with new changes, ex. changing background color
+
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0: #LEFT player yellow, checks if pressing key would send spaceship offscreen
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x: #RIGHT player yellow, checks if the right corner of spaceship tries to cross center border
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0: #UP player yellow
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < WINDOW_HEIGHT - 10: #DOWN player yellow
+        yellow.y += VEL
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width: #LEFT player red
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WINDOW_WIDTH: #RIGHT player red
+        red.x += VEL
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0: #UP player red
+        red.y -= VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < WINDOW_HEIGHT - 10: #DOWN player red
+        red.y += VEL
 
 
 def main():
+    red = pygame.Rect(WINDOW_WIDTH*3/4, WINDOW_HEIGHT/2, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(WINDOW_WIDTH*1/4, WINDOW_HEIGHT/2, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
+    red_bullets = []
+    yellow_bullets = []
+
     clock = pygame.time.Clock()
     run = True
     while(run):
@@ -62,7 +95,23 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False         #If the user closes the window, the service stops itself
-        draw_window()
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS: #Checks if there are less than max bullets from yellow on screen
+                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y, yellow.height/2 - 2, 10, 5)
+                    yellow_bullets.append(bullet)
+
+                if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(red.x, red.y, red.height/2 - 2, 10, 5)
+                    red_bullets.append(bullet)
+
+        keys_pressed = pygame.key.get_pressed() #gets all keys that are pressed down
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+
+        
+        
+        draw_window(red, yellow)
     
     
     pygame.quit()
